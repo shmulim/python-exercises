596eb1fd4a27b1225f799b1b30a2dc671daf9166
@shmulim <shmuel.markel@gmail.com>
2015-06-16 22:28:01 -0700

    Create 003.py

---
+++ unit4/003.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+now = datetime.now()
+current_year = now.year
+current_month = now.month
+current_day = now.day
+
+print now
+print current_year
+print current_month
+print current_day
