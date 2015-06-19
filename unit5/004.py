a591ab5f0ce121625accbed17aa6c8db7d8404c8
@shmulim <shmuel.markel@gmail.com>
2015-06-18 20:32:47 -0700

    Create 004.py

---
+++ unit5/004.py
@@ -0,0 +1,16 @@
+# Create comparative statements as appropriate on the lines below!
+
+# Make me true!
+bool_one = 3 < 5  # We already did this one for you!
+
+# Make me false!
+bool_two = 4 > 5
+
+# Make me true!
+bool_three = 2 + 2 == 4
+
+# Make me false!
+bool_four = 1 > 4
+
+# Make me true!
+bool_five = 1 < 4
